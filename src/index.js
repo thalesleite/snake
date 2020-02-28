713e3656bceb2e7637f2d8f95ab73665ed5086bc
thalesleite <thalesaleite@gmail.com>
2020-02-28 03:39:03 +0000

    added move for the snake

--- src/index.js
+++ src/index.js
@@ -8,4 +8,22 @@ const draw = () => {
     snake.update();
 }
 
-setInterval(draw, 10);
+const keyPressed = (e) => {
+    console.log(e.keyCode);
+    if ( e.keyCode === 37 ) {
+        // 37 LEFT
+        snake.direction(-1, 0);
+    } else if ( e.keyCode === 38 ) {
+        // 38 UP
+        snake.direction(0, -1);
+    } else if ( e.keyCode === 39 ) {
+        // 39 RIGHT
+        snake.direction(1, 0);
+    } else if ( e.keyCode === 40 ) {
+        // 40 DOWN
+        snake.direction(0, 1);
+    }
+}
+
+setInterval(draw, 300);
+window.addEventListener('keydown', keyPressed);
--- src/snake.js
+++ src/snake.js
@@ -2,22 +2,33 @@ class Snake {
     constructor(canvas){
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
+        this.scale = 20;
         this.x = 0;
         this.y = 0;
         this.xSpeed = 1;
         this.ySpeed = 0;
     }
 
-    update(){
-        this.x += this.xSpeed;
-        this.y += this.ySpeed;
+    direction(x, y) {
+        this.xSpeed = x;
+        this.ySpeed = y;
     }
 
-    show(){
+    update() {
+        if ( ((this.x + this.scale) < this.canvas.width /*&& ((this.x + this.scale) > 0 || (this.x + this.scale) === 0 )*/ ) &&
+             ((this.y + this.scale) < this.canvas.height /*|| (this.y) < 0*/ )) {
+                this.x += this.xSpeed * this.scale;
+                this.y += this.ySpeed * this.scale;
+        }
+    }
+
+    show() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.beginPath();
-        this.ctx.fillRect(this.x, this.y, 10, 10);
+
+        this.ctx.fillRect(this.x, this.y, this.scale, this.scale);
         this.ctx.fillStyle = '#fff';
+
         this.ctx.closePath();
     }
 }
